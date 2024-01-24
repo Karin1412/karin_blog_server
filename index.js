32c8ebc9e1dd9788c87f54c8ba6f6090ce3e741d
Karin1412 <100667390+Karin1412@users.noreply.github.com>
2024-01-24 20:54:57 +0700

    connect db

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import posts from './routers/posts.js'
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = process.env.PORT||5500;
+
+const URI = 'mongodb+srv://karin:syPlpBDbJoIUpGBU@cluster0.lwilyb2.mongodb.net/?retryWrites=true&w=majority'
 
 app.use(bodyParser.json({limit:'30mb'}));
 app.use(bodyParser.urlencoded({extended: true, limit: '30mb'}));
@@ -12,6 +15,12 @@ app.use(cors());
 
 app.use('/posts', posts)
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on PORT: ${PORT}`);
-});
+mongoose.connect(URI, {useNewURLParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('success');
+        app.listen(PORT, ()=>{
+            console.log(`Server is running on PORT: ${PORT}`);
+        });
+    }).catch(err => {
+        console.log('err', err)
+    })
