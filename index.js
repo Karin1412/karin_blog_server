4cac2bad483e16da697a4fb1dd994766d1d54e17
Karin1412 <100667390+Karin1412@users.noreply.github.com>
2024-01-24 22:15:40 +0700

    update api

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import posts from './routers/posts.js'
-import mongoose from "mongoose";
+import express from 'express';
+import cors from 'cors';
+import posts from './routers/posts.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT||5500;
+const PORT = process.env.PORT || 5000;
 
 const URI = 'mongodb+srv://karin:syPlpBDbJoIUpGBU@cluster0.lwilyb2.mongodb.net/?retryWrites=true&w=majority'
 
-app.use(bodyParser.json({limit:'30mb'}));
-app.use(bodyParser.urlencoded({extended: true, limit: '30mb'}));
 app.use(cors());
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 
-app.use('/posts', posts)
+app.use('/posts', posts);
 
-mongoose.connect(URI, {useNewURLParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('success');
-        app.listen(PORT, ()=>{
-            console.log(`Server is running on PORT: ${PORT}`);
-        });
-    }).catch(err => {
-        console.log('err', err)
-    })
+mongoose
+  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to DB');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log('err', err);
+  });
--- routers/posts.js
+++ routers/posts.js
@@ -1,8 +1,13 @@
-import express from 'express'
-import { getPosts } from '../controllers/posts.js';
+import express from 'express';
+import { getPosts, createPost, updatePost } from '../controllers/posts.js';
 
 const router = express.Router();
 
-router.get('/', getPosts)
+
+router.get('/', getPosts);
+
+router.post('/', createPost);
+
+router.post('/update', updatePost);
 
 export default router;
